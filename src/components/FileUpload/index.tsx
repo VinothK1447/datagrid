59b340a4a9830a5fcd5657280274720b3dc9680d
Vinothkumar Krishnamoorthy <Vinothkumar.k@envestnet.com>
2022-10-25 00:18:47 +0530

    Styling updates

--- src/components/FileUpload/index.tsx
+++ src/components/FileUpload/index.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { STATIC_STRINGS } from '../../utils/Constants'
 import Utils from '../../utils/Utils'
 import DataGrid from '../DataGrid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudArrowUp, faCircleCheck, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 
 const FileUploader = () => {
+	const fileInputBtn = useRef<any>()
 	const [csvFile, setCsvFile] = useState()
 	const [csvArray, setCsvArray] = useState<any>()
 	const [headers, setHeaders] = useState<string[]>([])
 	const [totalPages, setTotalPages] = useState(0)
+	const [fileStatus, setFileStatus] = useState<any>(null)
 
 	const submit = () => {
 		const file: any = csvFile
@@ -25,6 +29,11 @@ const FileUploader = () => {
 	const updateFile = (e: React.ChangeEvent) => {
 		let elem = e.target as any
 		const file = elem.files[0]
+		if (file) {
+			setFileStatus('success')
+		} else {
+			setFileStatus('error')
+		}
 		setCsvFile(file)
 	}
 
@@ -35,11 +44,20 @@ const FileUploader = () => {
 		}
 	}
 
+	const uploadFile = () => {
+		fileInputBtn.current.click()
+	}
+
 	return (
 		<>
 			<div className='upload-section'>
-				<input type='file' accept='.csv' onChange={updateFile} />
-				<button onClick={parseCSVFile}>{STATIC_STRINGS.PARSE_CSV_BTN_TXT}</button>
+				<input ref={fileInputBtn} type='file' accept='.csv' onChange={updateFile} className={'file-input'} />
+				<FontAwesomeIcon icon={faCloudArrowUp} size={'3x'} onClick={uploadFile} className={'file-input-icon'} />
+				{fileStatus && fileStatus === 'success' ? <FontAwesomeIcon icon={faCircleCheck} size={'2x'} className={'file-upload-success'} /> : null}
+				{fileStatus && fileStatus === 'error' ? <FontAwesomeIcon icon={faCircleXmark} size={'2x'} className={'file-upload-error'} /> : null}
+				<button onClick={parseCSVFile} className={'primary-btn'}>
+					{STATIC_STRINGS.PARSE_CSV_BTN_TXT}
+				</button>
 			</div>
 			<div className='datagrid-section'>{headers && csvArray && <DataGrid data={csvArray} headers={headers} bordered={true} totalPages={totalPages} />}</div>
 		</>
